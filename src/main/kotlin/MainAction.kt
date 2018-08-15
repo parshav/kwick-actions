4441a1e87d02520f22d3c0a5bd09d184ca3c1bea
Parshav <parshav@live.com>
2018-08-15 11:11:21 -0500

    Added slot handler

--- src/main/kotlin/MainAction.kt
+++ src/main/kotlin/MainAction.kt
@@ -1,8 +1,27 @@
+import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 
 class MainAction : AnAction() {
 
+    companion object {
+        const val SLOT_1 = "kwick.action.1"
+        const val SLOT_2 = "kwick.action.2"
+        const val SLOT_3 = "kwick.action.3"
+        const val SLOT_4 = "kwick.action.4"
+        const val SLOT_5 = "kwick.action.5"
+    }
+
     override fun actionPerformed(e: AnActionEvent) {
+        handleAction(ActionManager.getInstance().getId(this))
+    }
+
+    private fun handleAction(action: String) = when (action) {
+        SLOT_1 -> {}
+        SLOT_2 -> {}
+        SLOT_3 -> {}
+        SLOT_4 -> {}
+        SLOT_5 -> {}
+        else -> {}
     }
 }
