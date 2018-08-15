be0e52a068326c9520bd38a0e3cc2bdf36cd6e3f
Parshav <parshav@live.com>
2018-08-15 10:55:57 -0500

    Initial MainAction added

---
+++ src/main/kotlin/MainAction.kt
@@ -0,0 +1,8 @@
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class MainAction : AnAction() {
+
+    override fun actionPerformed(e: AnActionEvent) {
+    }
+}
